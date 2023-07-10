6f21d183ab859d6e9b4a24db070d139384b40a23
mina <anfruf0529@gmail.com>
2023-07-10 17:35:52 +0900

    -- useRef

--- src/InputTest.js
+++ src/InputTest.js
@@ -3,15 +3,15 @@
     @content : '벨로퍼트와 함께하는 모던 리액트'
     @order : 1
 */
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 function InputTest(){
     const [user, setUser] = useState({
         name : '',
         nick : ''
     });
-
     const { name, nick } = user;
+    const nameInput = useRef();
 
     const handleInput = (e) =>{
         const { name, value } = e.target;
@@ -26,11 +26,12 @@ function InputTest(){
             name : '',
             nick : '',
         })
+        nameInput.current.focus()
     }
 
     return (
         <div>
-            <input name="name" onChange={handleInput} value={name} placeholder="이름"/> 
+            <input name="name" onChange={handleInput} value={name} placeholder="이름" ref={nameInput}/> 
             <input name="nick" onChange={handleInput} value={nick} placeholder="닉네임"/> 
             <button onClick={resetBtn}>초기화</button>
             <p>{name}({nick})</p>
